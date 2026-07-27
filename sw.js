6ad0c9d1fed46b1f700265d3341d5c2ab04904ca
sylenovitorr-ux <sylenovitorr@gmail.com>
2026-07-27 15:53:56 -0400

    Atualiza cache offline para sincronização

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='qg-cbm-rr-v5';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='qg-cbm-rr-v6';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./supabase-sync.js','./supabase-sync.css','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html')))));
